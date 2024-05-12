59a559b7bc1af6d02f0f266b4b3443834e32c8aa
anand.kumar.m@philips.com <anand.kumar.m@philips.com>
2024-05-12 23:53:27 +0530

    private endpoint for web-app-slot

--- modules/win_web_app/frontend.tf
+++ modules/win_web_app/frontend.tf
@@ -15,11 +15,6 @@ resource "azurerm_windows_web_app" "frontendwebapp" {
   service_plan_id     = azurerm_service_plan.appserviceplan.id
 
   site_config {}
-
-  app_settings = {
-    "WEBSITE_DNS_SERVER" : "168.63.129.16",
-    "WEBSITE_VNET_ROUTE_ALL" : "1"
-  }
 }
 
 # Create Azure Private DNS Zone
@@ -48,6 +43,7 @@ resource "azurerm_private_endpoint" "privateendpoint" {
   }
 }
 
+
 #Create Deployment slot pre-production
 resource "azurerm_windows_web_app_slot" "deployment-slot" {
   name           = "pre-production"
@@ -62,12 +58,29 @@ resource "azurerm_app_service_virtual_network_swift_connection" "network-swift-c
   subnet_id      = var.isubnet
 }
 
+# Create Azure Private Endpoint for pre-production
+resource "azurerm_private_endpoint" "privateendpoint-pre-production" {
+  name                = "${var.privateendpoint}-pre-production"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_dns_zone_group {
+    name                 = "privatednszonegroup"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dnsprivatezone.id]
+  }
+
+  private_service_connection {
+    name                           = "privateendpointconnection-preproduction"
+    private_connection_resource_id = azurerm_windows_web_app.frontendwebapp.id
+    subresource_names              = ["sites-pre-production"]
+    is_manual_connection           = false
+  }
+}
+
 #Create App service slot virtual network connection
 resource "azurerm_app_service_slot_virtual_network_swift_connection" "slot-network-swift-connection" {
   slot_name      = azurerm_windows_web_app_slot.deployment-slot.name
   app_service_id = azurerm_windows_web_app.frontendwebapp.id
   subnet_id      = var.isubnet
 }
-
-
-
